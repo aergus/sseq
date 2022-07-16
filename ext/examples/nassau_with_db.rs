aab391921aaea85f3892bb4d205be905db6c1b3b
Aras Ergus <aras.ergus@epfl.ch>
2022-07-16 10:57:45 +0200

    Add temporary binary for tesing Postgres
    Please revert this before merging

---
+++ ext/examples/nassau_with_db.rs
@@ -0,0 +1,30 @@
+/// This is essentially a copy of `resolve_through_stem` using a database instead of files.
+///
+/// This is for testing purposes only, and should be removed before merging the database feature
+/// branch into `master`.
+use core::str::FromStr;
+
+use ext::{chain_complex::FreeChainComplex, save::SaveTarget, utils, utils::construct_nassau};
+
+fn main() -> anyhow::Result<()> {
+    let (name, module): (String, utils::Config) = query::with_default("Module", "S_2", |s| {
+        Result::<_, anyhow::Error>::Ok((s.to_owned(), s.try_into()?))
+    });
+
+    let save_target = query::optional(
+        "Connection string in the sense of https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING",
+        |s| postgres::Config::from_str(&s).map(SaveTarget::from),
+    );
+
+    let mut res = construct_nassau(module, save_target)?;
+    res.set_name(name);
+
+    let max_n = query::with_default("Max n", "30", str::parse);
+    let max_s = query::with_default("Max s", "15", str::parse);
+
+    res.compute_through_stem(max_s, max_n);
+
+    println!("{}", res.graded_dimension_string());
+
+    Ok(())
+}
